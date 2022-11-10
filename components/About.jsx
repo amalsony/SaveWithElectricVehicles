c3f321099191d6fc67dc6b3dbf298445209699fe
Amal Sony <amalsony@Amals-MacBook-Pro-2.local>
2022-11-10 13:49:17 +0530

    about

--- components/About.jsx
+++ components/About.jsx
@@ -7,12 +7,19 @@ export default function Calculator() {
       <div className={classes.aboutText}>
         <p>
           This website is a part of an IB Group 4 project by Amal, Ahaan,
-          Vedant, Ryan and Shriya to help people understand the benefits of
-          switching to an electric vehicle.
+          Vedant, Ayush, Ryan and Shriya to help people understand the benefits
+          of switching to an electric vehicle.
         </p>
         <p>
-          This open-source code was written by Amal Palackal and is available on
-          GitHub.
+          This open-source code was written by Amal Palackal and is available on{" "}
+          <a
+            target={"_blank"}
+            href="https://github.com/amalsony/SaveWithElectricVehicles"
+            className={classes.link}
+          >
+            GitHub
+          </a>
+          .
         </p>
       </div>
     </div>
